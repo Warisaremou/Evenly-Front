df2039d410da700c1a4f6a4aa2878e557fbe465a
Waris Aremou <warisaremou2003@gmail.com>
2025-01-22 02:16:01 +0100

    Update active dashboard page link

--- src/components/layouts/dashboard.tsx
+++ src/components/layouts/dashboard.tsx
@@ -1,10 +1,12 @@
-import CustomSidebarLink from "@/components/custom-sidebar-link";
 import Logo from "@/components/logo";
 import { siteConfig } from "@/config/site";
-import { Outlet } from "react-router";
+import { cn } from "@/lib/utils";
+import { Link, Outlet, useLocation } from "react-router";
 import LogoutButton from "../logout-button";
 
 export default function DashboardLayout() {
+  const location = useLocation();
+
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
@@ -12,12 +14,22 @@ export default function DashboardLayout() {
         <Logo />
         <div className="space-y-10 w-full">
           <ul className="w-full gap-2 flex flex-col pb-4 border-b border-grey-300">
-            {siteConfig.dashboardNav.map((item, index) => (
-              <CustomSidebarLink
-                item={item}
-                key={`${index}-${item.title}`}
-              />
-            ))}
+            {siteConfig.dashboardNav.map((item, index) => {
+              const isActive = location.pathname.includes(item.href);
+              return (
+                <Link
+                  key={`${index}-${item.title}`}
+                  className={cn(
+                    "rounded-lg flex items-center font-body-medium gap-2 h-10 px-4 py-2.5 text-sm text-grey-500 transition-colors duration-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-grey-300 focus-visible:ring-offset-0 hover:bg-primary-100",
+                    isActive && "bg-primary-100 text-primary-300",
+                  )}
+                  to={item.href}
+                >
+                  {item.icon && <item.icon className="size-5" />}
+                  {item.title}
+                </Link>
+              );
+            })}
           </ul>
           <LogoutButton />
         </div>
@@ -25,8 +37,8 @@ export default function DashboardLayout() {
 
       <div className="h-screen flex-1 overflow-y-scroll">
         {/* Header */}
-        <header className="sticky top-0 flex items-center justify-between gap-3 border-b border-grey-300 bg-grey-100 px-5 py-2.5">
-          <h3 className="font-body-medium text-grey-500">Empire Group</h3>
+        <header className="sticky top-0 flex items-center justify-between gap-3 border-b border-grey-300 bg-grey-100 px-5 py-3">
+          <h3 className="font-heading-semibold text-grey-500">Empire Group</h3>
         </header>
         {/* Children */}
         <div className="w-full mx-auto max-w-[50rem] max-lg:container max-lg:py-5 py-8">
