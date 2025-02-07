ddbd32b64167c92e10dc2abbaf306e8ebd514072
Waris Aremou <warisaremou2003@gmail.com>
2025-02-07 10:55:27 +0100

    Remove log

--- src/pages/dashboard/events/add-event.tsx
+++ src/pages/dashboard/events/add-event.tsx
@@ -3,10 +3,7 @@ import { APIProvider } from "@vis.gl/react-google-maps";
 
 export default function DashboardAddEvent() {
   return (
-    <APIProvider
-      apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-      onLoad={() => console.log("Maps API has loaded.")}
-    >
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <AddEditEventForm />
     </APIProvider>
   );
--- src/pages/dashboard/events/edit-event.tsx
+++ src/pages/dashboard/events/edit-event.tsx
@@ -1,9 +1,6 @@
 import { AddEditEventForm } from "@/components/forms";
-import { useParams } from "react-router";
 
 export default function DashboardEditEvent() {
-  const { id_event } = useParams();
-  console.log(id_event);
   return (
     <>
       <AddEditEventForm />
